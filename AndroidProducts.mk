fb627f3872ab59bfa3e7d7920271bf10ae107c49
vherawidatama <vherawidatama@gmail.com>
2023-07-10 09:48:20 +0000

    first commit fix play protect uncertified

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lunaa.mk
+    $(LOCAL_DIR)/derp_lunaa.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lunaa-user \
-    aosp_lunaa-userdebug \
-    aosp_lunaa-eng
+    derp_lunaa-user \
+    derp_lunaa-userdebug \
+    derp_lunaa-eng
--- derp_lunaa.mk
+++ derp_lunaa.mk
@@ -6,11 +6,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/lunaa/device.mk)
 
 # Inherit some common Elixir Stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Official-ify
-ELIXIR_MAINTAINER := X-R-P
-ELIXIR_BUILD_TYPE := OFFICIAL
+#ELIXIR_MAINTAINER := X-R-P
+#ELIXIR_BUILD_TYPE := OFFICIAL
 IS_PHONE := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_CALL_RECORDING := true
@@ -35,8 +35,9 @@ PRODUCT_SYSTEM_DEVICE := RE54ABL1
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="RMX3360-user 13 TP1A.220905.001 R.e9a1c9_63b61_5d8e0 release-keys" \
+    # Fix Safetynet - Play Protect Uncertified
+    PRIVATE_BUILD_DESC="redfin-user 13 TQ3A.230705.001 10216780 release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_NAME) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := realme/RMX3360/RE54ABL1:13/TP1A.220905.001/R.e9a1c9_63b61_5d8e0:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:13/TQ3A.230705.001/10216780:user/release-keys
